2f6dcf33ff46d8e716596d27b34bfb65ec2d82d7
soumyadurgam <soumyadurgam9489@gmail.com>
2024-11-21 09:54:35 +0000

    solution

---
+++ src/context/ThemeAndVideoContext.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ThemeAndVideoContext = React.createContext({
+  isDarkTheme: false,
+
+  savedVideos: [],
+
+  activeTab: 'Home',
+
+  toggleTheme: () => {},
+
+  changeTab: () => {},
+
+  addVideo: () => {},
+})
+
+export default ThemeAndVideoContext
